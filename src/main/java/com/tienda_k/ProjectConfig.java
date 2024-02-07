cdd1c3747e88d071b865a92482743d43717f14eb
Andres <andrescasth@gmail.com>
2024-02-07 16:17:18 -0600

    Clase semana 4, bootstrap e idiomas

--- src/main/java/com/tienda_k/ProjectConfig.java
+++ src/main/java/com/tienda_k/ProjectConfig.java
@@ -1,5 +1,5 @@
-
 package com.tienda_k;
+
 import java.util.Locale;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -9,31 +9,29 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
 import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
-@Configuration 
-public class ProjectConfig implements WebMvcConfigurer{
-    
-    
+@Configuration
+public class ProjectConfig implements WebMvcConfigurer {
+
     @Bean
-    public LocaleResolver localeResolver(){
+    public LocaleResolver localeResolver() {
         var slr = new SessionLocaleResolver();
         slr.setDefaultLocale(Locale.getDefault());
         slr.setLocaleAttributeName("session.current.locale");
         slr.setTimeZoneAttributeName("session.current.timezone");
         return slr;
-    }  
-    
+    }
+
     @Bean
-    public LocaleChangeInterceptor localeChangeInterceptor(){
-        var lcl= new LocaleChangeInterceptor();
-        lcl.setParamName("lang");
-        return lcl;
+    public LocaleChangeInterceptor localeChangeInterceptor() {
+        var lci = new LocaleChangeInterceptor();
+        lci.setParamName("lang");
+        return lci;
     }
     
     @Override
-    public void addInterceptor(interceptorRegistry registro){
+    public void addInterceptors(InterceptorRegistry registro) {
         registro.addInterceptor(localeChangeInterceptor());
-        
-}
-}
-    
+    }
 
+    
+}
